72981916fdbb278ca367eab91f648eaaa57b440e
UN <59855096+rattamnoon@users.noreply.github.com>
2023-12-24 14:21:51 +0700

    Create an AWS resource (ECR Repository)
    Signed-off-by: UN <59855096+rattamnoon@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("pulumi-s3-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+// Create an AWS resource (ECR Repository)
+const repository = new awsx.ecr.Repository("pulumi-ecr-repo", {
+  imageScanningConfiguration: {
+    scanOnPush: true,
+  },
+  forceDelete: true,
+});
+
+// Export the URL of the repository
+const image = new awsx.ecr.Image("image", {
+  repositoryUrl: repository.url,
+  context: "./app",
+  platform: "linux/amd64",
+});
+
+export const url = repository.url;
